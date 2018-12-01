d400cad5336bdd1efdf5d74a6c2e59e86d86797b
natan33 <srassis2014@gmail.com>
2018-11-30 22:14:01 -0200

    segundo commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 
-/**
- *
- * @author Aluno
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        Aluno a1 = new Aluno("Maria",31);
+        Aluno a2 = new Aluno("Maria",33);
+        Aluno a3 = new Aluno("Marta",45);
+         //para fazer list
+        List<Aluno> listaAluno = new ArrayList<>();
+//        listaAluno.add(a1);
+//        listaAluno.add(a2);
+//        listaAluno.add(a3);
+        //'ctrl + /' para comenta varias linhas
+        
+        listaAluno.addAll(Arrays.asList(a1,a2,a3));
+         
+        
+        //para ver a lista por inteiro
+      
+        for (Aluno listaAluno1 : listaAluno) {
+            System.out.println("Nome: "+listaAluno1.getNome()
+                    +" com idade: "+listaAluno1.getIdade()
+                    +" anos");
+            
+        }
     }
     
 }
